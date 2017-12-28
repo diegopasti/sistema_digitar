52c6887898225f6f135a1a87e6b7424adee4f6e9
Diego Pasti <diegopasti@gmail.com>
2017-12-28 01:49:55 -0200

    Removi imports repetidos na tela do controle de contratos. Minifiquei a versão atualizada do WorkingApi.

--- static/modules/honorary/honorary/js/honorary.js
+++ static/modules/honorary/honorary/js/honorary.js
@@ -12,9 +12,40 @@ function post_screen_verified(){
 }
 
 function configurar_formulario_padrao(){
-	//$("#competence").val('2')
-	//$("#tipos_proventos").val('C')
-	//$("#valor").maskMoney({showSymbol:false, symbol:"R$", decimal:",", thousands:"."});
+
+
+	var month_names = {0:'JAN',1:'FEV',2:'MAR',3:'ABR',4:'MAI',5:'JUN',6:'JUL',7:'AGO',8:'SET',9:'OUT',10:'NOV',11:'DEZ'}
+	var current_month = new Date().getMonth();
+	var year = new Date().getFullYear();
+	var array = [current_month+3,current_month+2,current_month+1,current_month,current_month-1,current_month-2,current_month-3,"TODOS"];
+	var selected = "";
+	var selectList = document.getElementById("competence");
+	for (var i = 0; i < array.length; i++) {
+			var option = document.createElement("option");
+			var item = "";
+
+			if(array[i] > 11){
+				item = month_names[array[i] - 12]+"/"+(year+1).toString();
+			}
+			else if(array[i] < 0){
+				item = month_names[array[i] + 12]+"/"+(year-1).toString();
+			}
+			else if(array[i] == "TODOS"){
+				item = "TODOS"
+			}
+			else{
+				item = month_names[array[i]]+"/"+year.toString();
+			}
+
+			option.value = item;
+			option.text = item;
+			if(array[i] == current_month){
+				selected = item
+			}
+
+			selectList.appendChild(option);
+	}
+	$("#competence").val(selected)
 }
 
 function resetar_formulario(){
