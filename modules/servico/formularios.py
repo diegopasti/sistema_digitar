2ff5d535931f501a790f68d91624f24c60403422
Diego Pasti <diegopasti@gmail.com>
2018-01-19 02:43:16 -0200

    Correção no cadastro dos serviços que não exibia o registro corretamente no formulário quando o mesmo era selecionado para alteração.

--- modules/servico/formularios.py
+++ modules/servico/formularios.py
@@ -14,10 +14,10 @@ MENSAGENS_ERROS={'required': 'Precisa ser Informado!',
 
 class formulario_adicionar_servico(forms.Form):
     servico      = forms.CharField(label="Serviço: ", max_length=100, required=True, error_messages=MENSAGENS_ERROS,
-                           widget=forms.TextInput(attrs={'class': 'form-control uppercase','id': 'modal_servico','ng-model':'modal_servico'}))
+                           widget=forms.TextInput(attrs={'class': 'form-control uppercase','id': 'modal_servico'}))
 
     descricao = forms.CharField(label="Descrição (Opcional): ",max_length=500,required=False,error_messages=MENSAGENS_ERROS,
-                                widget=forms.Textarea(attrs={'class':"form-control uppercase", 'id':'modal_descricao','ng-model':'modal_descricao' }))
+                                widget=forms.Textarea(attrs={'class':"form-control uppercase", 'id':'modal_descricao'}))
 
 class formulario_adicionar_plano(forms.Form):
     plano      = forms.CharField(label="Nome", max_length=100, required=True, error_messages=MENSAGENS_ERROS,
