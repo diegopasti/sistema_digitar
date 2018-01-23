41111b40863d3212dd6131ad56ba478b5059c61f
Cleiton Leonel <11451914+cleitonleonel@users.noreply.github.com>
2018-01-23 10:53:10 -0200

    Update api.py

--- modules/core/api.py
+++ modules/core/api.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import Http404
 from django.utils.decorators import method_decorator
 from libs.backup.backup import BackupManager
-from libs.backup.pygit import check_update, update
+from libs.backup.pygit import check_update, update, install
 from libs.default.core import BaseController
 from modules.nucleo.models import Backup
 #from modules.user.models import User
@@ -186,6 +186,7 @@ class ConfigurationsController(BaseController):
     def update(self,request):
         self.start_process(request)
         updating = update()
+        install_pack = install()
 
     def shared_folder(self,request):
         self.start_process(request)
